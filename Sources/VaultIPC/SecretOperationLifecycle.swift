c9197f97cb1c307636a64315cd23173e40d63548
zyk1172 <53221608+zyk1172@users.noreply.github.com>
2026-09-13 05:44:52 +0800

    refactor: add operation lifecycle wire models

---
+++ Sources/VaultIPC/SecretOperationLifecycle.swift
@@ -0,0 +1,50 @@
+import Foundation
+import VaultCore
+
+public enum SecretOperationState: String, Codable, Equatable, Sendable {
+    case queued
+    case awaitingApproval
+    case running
+    case succeeded
+    case failed
+    case cancelled
+    case outcomeUnknown
+
+    public var isTerminal: Bool {
+        switch self {
+        case .succeeded, .failed, .cancelled, .outcomeUnknown:
+            return true
+        case .queued, .awaitingApproval, .running:
+            return false
+        }
+    }
+}
+
+public struct SecretOperationHandle: Codable, Equatable, Sendable {
+    public let operationID: UUID
+    public let state: SecretOperationState
+
+    public init(operationID: UUID, state: SecretOperationState) {
+        self.operationID = operationID
+        self.state = state
+    }
+}
+
+public struct SecretOperationStatus: Codable, Equatable, Sendable {
+    public let operationID: UUID
+    public let state: SecretOperationState
+    public let output: SecretOperationOutput?
+    public let errorCode: String?
+
+    public init(
+        operationID: UUID,
+        state: SecretOperationState,
+        output: SecretOperationOutput? = nil,
+        errorCode: String? = nil
+    ) {
+        self.operationID = operationID
+        self.state = state
+        self.output = output
+        self.errorCode = errorCode
+    }
+}
