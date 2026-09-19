969dc889741a2f4ac8de990f8d2cd13c85c1b91a
zhengyk <53221608+zyk1172@users.noreply.github.com>
2026-09-19 08:59:49 +0800

    feat: expose operation-not-found lifecycle error

--- Sources/VaultCore/Models/SecretOperation.swift
+++ Sources/VaultCore/Models/SecretOperation.swift
@@ -293,6 +293,7 @@ public enum SecretOperationError: Error, Equatable, Sendable {
     case redirectRequiresReview
     case outputQuarantined
     case insecureTransportDenied
+    case operationNotFound
     case idempotencyKeyConflict
 
     public var responseCode: String {
@@ -331,6 +332,8 @@ public enum SecretOperationError: Error, Equatable, Sendable {
             return "ACTION_OUTPUT_QUARANTINED"
         case .insecureTransportDenied:
             return "INSECURE_TRANSPORT_DENIED"
+        case .operationNotFound:
+            return "OPERATION_NOT_FOUND"
         case .idempotencyKeyConflict:
             return "IDEMPOTENCY_KEY_CONFLICT"
         }
